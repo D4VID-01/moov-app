bf5df9f8364cb50c9a0ff45c71d8860fe6fbc748
luisDavid <ld0110mp@gmail.com>
2025-09-23 19:51:33 -0600

    feat: add validation middleware in hadle.validation.js

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,11 +1,12 @@
 import { Router } from "express";
 import {register, login, logout} from "../controllers/auth.controller.js";
 import { registerValidtor, loginValidator } from "../validators/auth.validator.js";
+import { handleValidation } from "../middlewares/handle.validation.js";
 
 const router = Router();
 
-router.post('/register', registerValidtor, register);
-router.post('/login', loginValidator, login);
+router.post('/register', registerValidtor, handleValidation, register);
+router.post('/login', loginValidator, handleValidation, login);
 router.post('/logout', logout);
 
 export default router;
